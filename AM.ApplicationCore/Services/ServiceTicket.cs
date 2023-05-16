5bba07270d166868bceaf18728b93e9e5f8b773e
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-05-17 00:45:55 +0100

    project done !

---
+++ AM.ApplicationCore/Services/ServiceTicket.cs
@@ -0,0 +1,19 @@
+﻿using AM.ApplicationCore.Services;
+using AM.ApplicationCore.Interfaces;
+using AM.ApplicationCore.Domain;
+using System.Text.RegularExpressions;
+
+namespace AM.ApplicationCore.Interfaces
+{
+    public class ServiceTicket : Service<Ticket>, IServiceTicket
+    {
+        public ServiceTicket(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public IEnumerable<Ticket> GetTicketByPrice(string prix)
+        {
+            return GetMany(m => m.Prix >= float.Parse(prix));
+        }
+    }
+}
